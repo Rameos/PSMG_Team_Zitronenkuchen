ee9f02b27d031cbb2bb8261fe8506ae33db064a5
FabiBr <fabian.br.1992@gmail.com>
2014-04-19 10:13:16 +0200

    adds first version of gamefield creator

---
+++ PSMG_Team_Zitronenkuchen/UnityStuff_FabiBr/Strategiespiel/Assets/Materials/Skripts/CreateGameField.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateGameField : MonoBehaviour {
+
+
+    public GameObject hexagon;
+    public Transform firstField;
+
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < 20; i++)
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        
+                        Vector3 newposition = firstField.position;
+                        if(i != 0 || j != 0) {
+                        if (i % 2 == 0)
+                            {
+                                newposition.x += i * 100-20*i;
+                                newposition.z -= j * 100;
+                            }
+                        else
+                            {
+                                newposition.x += i * 100 -20*i;
+                                newposition.z -= j * 100 -50;
+                            }
+                        }
+                        
+
+
+                        Instantiate(hexagon,newposition,firstField.rotation);
+                    }
+                }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+	}
+}
