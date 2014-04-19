171dd89d554d0a81462fcac5ffcb2cb682ed7e70
FabiBr <fabian.br.1992@gmail.com>
2014-04-19 18:46:24 +0200

    updates the gamefield creator

--- PSMG_Team_Zitronenkuchen/UnityStuff_FabiBr/Strategiespiel/Assets/Materials/Skripts/CreateGameField.cs
+++ PSMG_Team_Zitronenkuchen/UnityStuff_FabiBr/Strategiespiel/Assets/Materials/Skripts/CreateGameField.cs
@@ -7,34 +7,32 @@ public class CreateGameField : MonoBehaviour {
     public GameObject hexagon;
     public Transform firstField;
 
+    private const float gapSize = 5;
+
 
 	// Use this for initialization
 	void Start () {
         for (int i = 0; i < 20; i++)
-                {
+                {       
                     for (int j = 0; j < 10; j++)
                     {
-                        
                         Vector3 newposition = firstField.position;
-                        if(i != 0 || j != 0) {
+                        if(!(i == 0 && j == 0)) {
                         if (i % 2 == 0)
                             {
-                                newposition.x += i * 100-20*i;
-                                newposition.z -= j * 100;
+                                newposition.x += i*(75+ gapSize);
+                                newposition.z -= j*(85+gapSize);
                             }
                         else
                             {
-                                newposition.x += i * 100 -20*i;
-                                newposition.z -= j * 100 -50;
+                                newposition.x += i*(75+ gapSize);
+                                newposition.z -= j * (85+gapSize) + 43;
                             }
+                            Instantiate(hexagon,newposition,firstField.rotation);
                         }
-                        
-
-
-                        Instantiate(hexagon,newposition,firstField.rotation);
                     }
                 }
-	}
+	        }
 	
 	// Update is called once per frame
 	void Update () {
