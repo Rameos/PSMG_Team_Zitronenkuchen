720ef62eae569dd448cf2a92bf6fe25836363049
FabiBr <fabian.br.1992@gmail.com>
2014-04-28 16:12:15 +0200

    updates the gamefield creator

--- PSMG_Team_Zitronenkuchen/UnityStuff_FabiBr/Strategiespiel/Assets/Materials/Skripts/CreateGameField.cs
+++ PSMG_Team_Zitronenkuchen/UnityStuff_FabiBr/Strategiespiel/Assets/Materials/Skripts/CreateGameField.cs
@@ -7,11 +7,11 @@ public class CreateGameField : MonoBehaviour {
     public GameObject hexagon;
     public GameObject field;
 
-    private const float GAP_SIZE = 3; // 0-5 seems reasonable
-    private const float FIELD_SIZE = 20; // startmenu function to select the fieldsize?
+    private const float GAP_SIZE = 1; // 0-5 seems reasonable
+    private const float FIELD_SIZE = 40; // startmenu function to select the fieldsize?
 
     private Quaternion FIELD_ROTATION = new Quaternion(0, 0, 0, 0);
-    private Vector3 HEX_SIZE = new Vector3(990, 990, 3990);
+    private Vector3 HEX_SIZE = new Vector3(500, 500, 2000);
     private Vector3 FIRSTHEXAGON_POSITION = new Vector3(250, 0,-650);
     private Vector3 ROTATION = new Vector3(90, 30, 0);
     private Vector3 newHexPosition;
@@ -39,7 +39,7 @@ public class CreateGameField : MonoBehaviour {
     private void addComponentsAndScale(GameObject hex)
     {
         hex.transform.parent = field.transform;
-        hex.transform.localScale += HEX_SIZE;
+        hex.transform.localScale = HEX_SIZE;
         hex.transform.Rotate(ROTATION);
         hex.AddComponent("ChangeFieldStateOnClick");
         hex.AddComponent("SphereCollider");
@@ -52,13 +52,13 @@ public class CreateGameField : MonoBehaviour {
         Vector3 newPosition = FIRSTHEXAGON_POSITION;
         if (i % 2 == 0)
             {
-                newPosition.x += i*(75+ GAP_SIZE);      // changes the position of every even numbered field dependent on the position of the first field
-                newPosition.z -= j*(85+GAP_SIZE);       //  
+                newPosition.x += i*(HEX_SIZE.x/13.3333f + GAP_SIZE);        // changes the position of every even numbered field dependent on the position of the first field
+                newPosition.z -= j*(HEX_SIZE.x/11.7647f + GAP_SIZE);        //
             }
         else
             {
-                newPosition.x += i*(75+ GAP_SIZE);          // changes the position of every odd numbered hexagon adding a slight offset so the field lines up properly
-                newPosition.z -= j * (85+GAP_SIZE) + 43;    //
+                newPosition.x += i * (HEX_SIZE.x / 13.3333f + GAP_SIZE);                          // changes the position of every odd numbered hexagon adding a slight offset so the field lines up properly
+                newPosition.z -= j * (HEX_SIZE.x / 11.7647f + GAP_SIZE) + HEX_SIZE.x/23.5294f;    //
             }
         return newPosition;
     }
