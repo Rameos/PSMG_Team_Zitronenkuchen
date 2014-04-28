03bd7bd7f0ef675f837bfe2d979a5ffbe776bdb7
einworB <philip.braun@stud.uni-regensburg.de>
2014-04-28 13:49:23 +0200

    created gaze_test scene with dummy hex-field; deactivated(locked) mouse cursor and activated gaze cursor(file/build&run to show properly, gaze cursor not accurate in unity preview window); fix #2

---
+++ PSMG_Team_Zitronenkuchen/Assets/Scripts/ShowGazeCursor.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+using iViewX;
+
+public class ShowGazeCursor : MonoBehaviour
+{
+
+    
+
+    //Position and Texture for the Gaze
+    public Texture2D gazeCursor;
+
+    
+    private bool isGazeCursorActive = false;
+
+    void Start()
+    {
+        Screen.lockCursor = true;
+    }
+
+    void OnGUI()
+    {
+        Vector3 posGaze = (gazeModel.posGazeLeft + gazeModel.posGazeRight) * 0.5f;
+        GUI.DrawTexture(new Rect(posGaze.x, posGaze.y, gazeCursor.width, gazeCursor.height), gazeCursor);
+        
+    }
+}
