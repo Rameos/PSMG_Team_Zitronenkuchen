3bb5b2cad60fe35f2a8c223b8a75edca87643fa9
JanaH <jana.helgath@stud.uni-regensburg.de>
2014-05-18 23:38:09 +0200

    added main menu

---
+++ PSMG_Team_Zitronenkuchen/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    public Texture backgroundTexture;
+
+    public float btnPosX1;
+    public float btnPosX2;
+    public float btnPosY1;
+    public float btnPosY2;
+    public float btnHeight1;
+    public float btnHeight2;
+    public float btnWidth1;
+    public float btnWidth2;
+
+    public GUISkin myGuiSkin;
+
+    void OnGUI()
+    {
+        // draw background
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), backgroundTexture);
+
+        //draw buttons
+        GUI.skin = myGuiSkin;
+
+        if (GUI.Button(new Rect(Screen.width * btnPosX1, Screen.height * btnPosY1, Screen.width * btnWidth1, Screen.height * btnHeight1), ""))
+        {
+            Application.LoadLevel("Create_Gamefield");
+        }
+        if (GUI.Button(new Rect(Screen.width * btnPosX2, Screen.height * btnPosY2, Screen.width * btnWidth2, Screen.height * btnHeight2), ""))
+        {
+            //options here
+        }
+    }
+	
+}
