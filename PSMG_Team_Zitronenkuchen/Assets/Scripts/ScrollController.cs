62a075b7eb1fb6e1670a54a79986ebe9767f5a52
BoNatali <Natali.Bopp@stud.uni-regensburg.de>
2014-05-19 23:26:27 +0200

    placed arrows on gamefield

---
+++ PSMG_Team_Zitronenkuchen/Assets/Scripts/ScrollController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using iViewX;
+
+public class ScrollController : MonoBehaviourWithGazeComponent
+{
+
+	// Use this for initialization
+	void Start () {
+       
+	}
+
+    void moveToLayer(Transform root, int layer)
+    {
+        root.gameObject.layer = layer;
+        foreach (Transform child in root)
+            moveToLayer(child, layer);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public override void OnGazeEnter(RaycastHit hit)
+    {
+        Debug.Log("Helloooo!!");
+        GameObject field = GameObject.FindGameObjectWithTag("Field");
+        int layer = LayerMask.NameToLayer("Ignore Raycast");
+        moveToLayer(field.transform, layer);
+        highlightMaterial();
+    }
+
+    public override void OnGazeStay(RaycastHit hit)
+    {
+        float currentTime = Time.time;
+        Debug.Log(Time.time);
+        Debug.Log(gameObject);
+        GameObject leftArrow = GameObject.FindGameObjectWithTag("LeftArrow");
+        GameObject rightArrow = GameObject.FindGameObjectWithTag("RightArrow");
+        GameObject upArrow = GameObject.FindGameObjectWithTag("UpArrow");
+        GameObject downArrow = GameObject.FindGameObjectWithTag("DownArrow");
+    }
+
+    public override void OnGazeExit()
+    {
+        resetMaterial();
+    }
+
+    private void highlightMaterial()
+    {
+        gameObject.transform.renderer.material.shader = Shader.Find("Self-Illumin/Outlined Diffuse");
+    }
+
+
+    private void resetMaterial()
+    {
+        gameObject.transform.renderer.material.shader = Shader.Find("Diffuse");
+    }
+
+
+}
