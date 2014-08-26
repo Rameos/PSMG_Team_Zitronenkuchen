7fe925a2a8b499defb96b5743ab5f2bf8beda65e
BoNatali <Natali.Bopp@stud.uni-regensburg.de>
2014-08-26 13:20:11 +0200

    economyECO states added

--- PSMG_Team_Zitronenkuchen/Assets/Scripts/HexField.cs
+++ PSMG_Team_Zitronenkuchen/Assets/Scripts/HexField.cs
@@ -180,9 +180,9 @@ public class HexField : MonoBehaviour {
         }
         else
         {
-            milBuildingState1 = Resources.Load("militaryECONOMYState1", typeof(GameObject)) as GameObject;
-            milBuildingState2 = Resources.Load("militaryECONOMYState2", typeof(GameObject)) as GameObject; 
-            milBuildingState3 = Resources.Load("militaryECONOMYState3", typeof(GameObject)) as GameObject;
+            milBuildingState1 = Resources.Load("militaryECOState1", typeof(GameObject)) as GameObject;
+            milBuildingState2 = Resources.Load("militaryECOState2", typeof(GameObject)) as GameObject; 
+            milBuildingState3 = Resources.Load("militaryECOState3", typeof(GameObject)) as GameObject;
         }       
         GameObject militaryBuildingState1 = Instantiate(milBuildingState1, selectedHexagon.transform.position, new Quaternion(0.0f, 0.0f, 0.0f, 0.0f)) as GameObject;
         GameObject militaryBuildingState2 = Instantiate(milBuildingState2, selectedHexagon.transform.position, new Quaternion(0.0f, 0.0f, 0.0f, 0.0f)) as GameObject;
