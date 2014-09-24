a78200fa9f67815be833e4c92f12622e22b193c6
BoNatali <Natali.Bopp@stud.uni-regensburg.de>
2014-09-24 17:13:58 +0200

    added tutorial scene with intro

---
+++ PSMG_Team_Zitronenkuchen/Assets/Scripts/TextMovementBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextMovementBehaviour : MonoBehaviour {
+
+    Vector3 destination;
+    Vector3 start;
+    private float startTime;
+    private float movementLength;
+	// Use this for initialization
+	void Start () {
+      destination = new Vector3(0.0216f, 1.4106f, -9.531f);
+      start = gameObject.transform.position;
+
+      startTime = Time.time;
+      movementLength = Vector3.Distance(start, destination);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float distCovered = (Time.time - startTime) * 0.05f;
+        float fracJourney = distCovered / movementLength;
+        gameObject.transform.position = Vector3.Lerp(start, destination, fracJourney);
+	}
+}
---
+++ PSMG_Team_Zitronenkuchen/Assets/Scripts/TutorialNavigation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialNavigation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI () {
+        if (Event.current.type == EventType.KeyDown && Event.current.keyCode == KeyCode.Space)
+        {
+            Destroy(GameObject.FindGameObjectWithTag("IntroText"));
+            Destroy(GameObject.FindGameObjectWithTag("IntroStars"));
+            Destroy(GameObject.FindGameObjectWithTag("SkipText"));
+        }
+    }
+     
+}
